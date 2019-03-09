410b2232149d5145ab8a36c215a232490cc37167
Temme <ddesantiago0@gmail.com>
2019-03-09 12:22:37 -0600

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+  //Define word to be guess by user
+
+  var hiddenWordArr = ["cosmo", "seinfield", "elaine", "george", "newman"];
+
+  var hiddenWord = hiddenWordArr[Math.floor(Math.random() * hiddenWordArr.length)];
+
+  var answerArray = [ ];
+
+  var countR = 1;
+
+  var countG = 0;
+
+  //Define the underscores from the hidden word
+
+  for (var i = 0; i < hiddenWord.length; i ++){
+      
+      answerArray[i] = "_";
+
+  }
+
+  //Input the underscore lenght to the html
+
+  document.getElementById("answer").innerHTML = answerArray;
+
+  console.log(answerArray[i]);
+
+  //Start the game with the input from the user
+
+  document.onkeyup = function(event) {
+
+    //Storage the selection of the user on a variable
+
+        var userGuess = event.key;
+
+        console.log(userGuess);
+
+    //Run a loop for each letter of the variable in the array
+
+    for (var i = 0; i < hiddenWord.length; i++){
+
+    //Verify if the letter guesses by the user is the same as the hidden word
+
+            if(hiddenWord[i] === userGuess){
+
+                answerArray[i] = userGuess;
+
+                console.log(hiddenWord[i]);
+
+                document.getElementById("answer").innerHTML = answerArray.join(" ");
+
+                countG++;
+
+                document.getElementById("lettGuess").innerHTML = countG;
+
+            } 
+
+            //Message when the number of counts reach 9
+
+            else {
+
+                document.getElementById("numGuess").innerHTML = 9 - countR;
+
+            };
+        };
+    }
