1dff8926034b20cac0e7986b965b92169284d660
Temme <ddesantiago0@gmail.com>
2019-03-09 13:35:16 -0600

    casi listo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,128 @@
+//Define word to be guess by user
+
+var hiddenWordArr = ["cosmo", "seinfield", "elaine", "george", "newman"];
+
+var hiddenWord = hiddenWordArr[Math.floor(Math.random() * hiddenWordArr.length)];
+
+var answerString = "";
+
+var answerArray = [];
+
+var countR = 1;
+
+var countG = 0;
+
+//Define the underscores from the hidden word
+
+for (var i = 0; i < hiddenWord.length; i ++){
+    
+  answerString += "_ ";
+  answerArray.push("");
+
+}
+
+//Input the underscore lenght to the html
+
+document.getElementById("answer").innerHTML = answerString;
+
+console.log(answerString);
+
+//Start the game with the input from the user
+
+document.onkeyup = function(event) {
+
+  //Storage the selection of the user on a variable
+
+      var userGuess = event.key;
+
+      console.log(userGuess);
+
+  //Run a loop for each letter of the variable in the array
+
+  for (var i = 0; i < hiddenWord.length; i++){
+
+  //Verify if the letter guesses by the user is the same as the hidden word
+
+          if(hiddenWord[i] === userGuess){
+
+              answerArray[i] = userGuess;
+
+              console.log(hiddenWord[i]);
+
+              //document.getElementById("answer").innerHTML = answerArray.join(" ");
+
+              //countG++;
+
+              //document.getElementById("lettGuess").innerHTML = countG;
+
+          } 
+
+          //Message when the number of counts reach 9
+
+          else {
+
+              document.getElementById("numGuess").innerHTML = 9 - countR;
+
+          };
+      };
+  }
+
+
+  
   //Define word to be guess by user
 
-  var hiddenWordArr = ["cosmo", "seinfield", "elaine", "george", "newman"];
+  // var hiddenWordArr = ["cosmo", "seinfield", "elaine", "george", "newman"];
 
-  var hiddenWord = hiddenWordArr[Math.floor(Math.random() * hiddenWordArr.length)];
+  //var hiddenWord = hiddenWordArr[Math.floor(Math.random() * hiddenWordArr.length)];
 
-  var answerArray = [ ];
+  //var answerArray = [ ];
 
-  var countR = 1;
+  //var countR = 1;
 
-  var countG = 0;
+  //var countG = 0;
 
   //Define the underscores from the hidden word
 
-  for (var i = 0; i < hiddenWord.length; i ++){
-      
-      answerArray[i] = "_";
-
-  }
+  //for (var i = 0; i < hiddenWord.length; i ++){answerArray[i] = "_";}
 
   //Input the underscore lenght to the html
 
-  document.getElementById("answer").innerHTML = answerArray;
+  //document.getElementById("answer").innerHTML = answerArray;
 
-  console.log(answerArray[i]);
+  //console.log(answerArray[i]);
 
   //Start the game with the input from the user
 
-  document.onkeyup = function(event) {
+  //document.onkeyup = function(event) {
 
     //Storage the selection of the user on a variable
 
-        var userGuess = event.key;
+//        var userGuess = event.key;
 
-        console.log(userGuess);
+  //      console.log(userGuess);
 
     //Run a loop for each letter of the variable in the array
 
-    for (var i = 0; i < hiddenWord.length; i++){
+    //for (var i = 0; i < hiddenWord.length; i++){
 
     //Verify if the letter guesses by the user is the same as the hidden word
 
-            if(hiddenWord[i] === userGuess){
+      //      if(hiddenWord[i] === userGuess){
 
-                answerArray[i] = userGuess;
+        //        answerArray[i] = userGuess;
 
-                console.log(hiddenWord[i]);
+          //      console.log(hiddenWord[i]);
 
-                document.getElementById("answer").innerHTML = answerArray.join(" ");
+            //    document.getElementById("answer").innerHTML = answerArray.join(" ");
 
-                countG++;
+              //  countG++;
 
-                document.getElementById("lettGuess").innerHTML = countG;
-
-            } 
+                //document.getElementById("lettGuess").innerHTML = countG;} 
 
             //Message when the number of counts reach 9
 
-            else {
+           // else {
 
-                document.getElementById("numGuess").innerHTML = 9 - countR;
-
-            };
-        };
-    }
+  //              document.getElementById("numGuess").innerHTML = 9 - countR;};
+ //       };
+ //   }
